6aa6b07674fc62b7dabf01db0574c456124358db
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-08-30 19:41:32 -0300

    Mejorado y fixeado ejercicio de regfile

--- SingleCycleProcessor/regfile.sv
+++ SingleCycleProcessor/regfile.sv
@@ -7,19 +7,46 @@ module regfile
 	output logic [63 : 0] rd1, rd2
 );
 	
-	// registers array
-	logic [63 : 0] X [0 : 31];
-
-	initial begin
-		// init registers
-		X[31] = 0;
-		for(int i = 0; i < 31; i++)
-			X[i] = i;
-	end
+	// registers array and init
+	logic [63 : 0] X [0 : 31] = 
+		'{
+			64'd0,
+			64'd1,
+			64'd2,
+			64'd3,
+			64'd4,
+			64'd5,
+			64'd6,
+			64'd7,
+			64'd8,
+			64'd9,
+			64'd10,
+			64'd11,
+			64'd12,
+			64'd13,
+			64'd14,
+			64'd15,
+			64'd16,
+			64'd17,
+			64'd18,
+			64'd19,
+			64'd20,
+			64'd21,
+			64'd22,
+			64'd23,
+			64'd24,
+			64'd25,
+			64'd26,
+			64'd27,
+			64'd28,
+			64'd29,
+			64'd30,
+			64'd0
+		};
 	
 	// read (async)
-	assign rd1 = (we3 && wa3 !== 31 ? wd3 : X[ra1]);
-	assign rd2 = (we3 && wa3 !== 31 ? wd3 : X[ra2]);
+	assign rd1 = X[ra1];
+	assign rd2 = X[ra2];
 	
 	// write (sync)
 	always @(posedge clk)
--- SingleCycleProcessor/regfile_tb.sv
+++ SingleCycleProcessor/regfile_tb.sv
@@ -1,10 +1,19 @@
-// Registers test bench module
+// Registers module
 module regfile_tb
 	();
-
-	parameter CNT_TESTS = 64;
 	
+	// Parameters
+	parameter CNT_TESTS = 64; // cnt of tests
+	
+	// Test bench variables
 	logic clk, reset_tb;
+	logic [63 : 0] rd1_expected, rd2_expected, wd3_auxiliar;
+	
+	int test_number, cnt_errors;
+	logic [207 : 0] test [0 : CNT_TESTS-1];
+		// {{we3}, {ra1}, {ra2}, {wa3}, {wd3}, {rd1_expected}, {rd2_expected}}
+	
+	// Module connections
 	logic we3;
 	logic [4 : 0] ra1, ra2, wa3;
 	logic [63 : 0] wd3, rd1, rd2;
@@ -21,54 +30,57 @@ module regfile_tb
 			.rd2(rd2)
 		);
 
-	// init clock
+	// Clock generation with 10ns period
 	always begin
-		clk = 1; #10ns;
-		clk = 0; #10ns;
+		clk = 1; #5ns;
+		clk = 0; #5ns;
 	end
-
-	logic [207 : 0] test [0 : (CNT_TESTS-1)]; // {{we3}, {ra1}, {ra2}, {wa3}, {wd3}, {rd1_expected}, {rd2_expected}}
-	logic [63 : 0] rd1_expected, rd2_expected, wd3_auxiliar;
-	int test_number, cnt_errors;
-	 
+	
 	initial begin
-		// init tests
+		// Init tests
 		test_number = 0;
 		cnt_errors = 0;
 		
 		// test-check initialization
 		for(int i = 0; i < 32; i++) begin
 			wd3_auxiliar = {2{32'($urandom())}};
-			test[i] = {{1'b0}, {5'(i)}, {5'(i)}, {5'(i)}, {wd3_auxiliar}, {2{(i === 0 || i === 31 ? 64'd0 : 64'(i))}}};
+			test[i] = {{1'b0}, {3{5'(i)}}, {wd3_auxiliar}, {2{(i === 0 || i === 31 ? 64'd0 : 64'(i))}}};
 		end
 		
 		// test-check writing
 		for(int i = 0; i < 32; i++) begin
 			wd3_auxiliar = {2{32'($urandom())}};
-			test[i + 32] = {{1'b1}, {5'(i)}, {5'(i)}, {5'(i)}, {wd3_auxiliar}, {2{(i !== 31 ? wd3_auxiliar : 64'b0)}}};
+			test[i + 32] = {{1'b1}, {3{5'(i)}}, {wd3_auxiliar}, {2{(i !== 31 ? wd3_auxiliar : 64'b0)}}};
 		end
 		
+		// First reset test bench creation
 		reset_tb = 1; #27ns reset_tb = 0;
 	end
-	 
-	// apply tests on rising edge of clk
-	always @(posedge clk)
-		#1 {we3, ra1, ra2, wa3, wd3, rd1_expected, rd2_expected} = test[test_number];
 	
-	// check tests on falling edge of clk
 	always @(negedge clk) begin
-		if(~reset_tb) begin;				
-			// check reading
-			if(rd1 !== rd1_expected || rd2 !== rd2_expected) begin
-				$display("Error in test number %d with input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and output = { rd1 = %d, rd2 = %d } --> The expected output was { rd1_expected = %d, rd2_expected = %d }", test_number, we3, ra1, ra2, wa3, wd3, rd1, rd2, rd1_expected, rd2_expected);
+		// Check test executed on positive edge of clk
+		if(~reset_tb) begin;
+			// Test writing (reading the written register)
+			if(test_number >= 32)
+				we3 = 1'b0;
+		
+			if({rd1, rd2} !== {rd1_expected, rd2_expected}) begin
+				if(test_number < 32)
+					$display("Error in initialization test number %d with input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and output = { rd1 = %d, rd2 = %d } --> The expected output was { rd1_expected = %d, rd2_expected = %d }", test_number, we3, ra1, ra2, wa3, wd3, rd1, rd2, rd1_expected, rd2_expected);
+				else
+					$display("Error in writing test number %d with input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and output = { rd1 = %d, rd2 = %d } --> The expected output was { rd1_expected = %d, rd2_expected = %d }", test_number, 1'b1, ra1, ra2, wa3, wd3, rd1, rd2, rd1_expected, rd2_expected);
 				cnt_errors++;
 			end
 			test_number++;
+			
 			if(test_number === CNT_TESTS) begin
-				$display("%d tests completed with %d errors", test_number, cnt_errors);
-				$stop;
+				$display("%d tests completed with %d errors", CNT_TESTS, cnt_errors);
+				#5ns $stop;
 			end
 		end
+		
+		// Prepare next test to positive edge of clk
+		#2ns {we3, ra1, ra2, wa3, wd3, rd1_expected, rd2_expected} = test[test_number]; #2ns;
 	end
 	
 endmodule
