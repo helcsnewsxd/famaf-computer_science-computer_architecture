68893a9d6b3ebbac56f290fdcddab7fb8057f7c4
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-09-01 13:15:07 -0300

    Mejorado test bench de signext

---
+++ SingleCycleProcessor/simple-modules/test-benches/input-files/signext_tb_in_gen.py
@@ -0,0 +1,42 @@
+from random import getrandbits, randint
+
+def twoComplement(number, cntBits): # for N bits
+    if number >= 0:
+        return number
+    return (~(-number)+1) + (1 << cntBits)
+
+def setCntBits(number, cntBits):
+    return number & ((1 << cntBits) - 1)
+
+def gen(test_number, negative):
+    # LDUR, STUR
+    if test_number == 0b111_1100_0010 or test_number == 0b111_1100_0000:
+        imm = (negative << 8) + getrandbits(8)
+        a = (test_number << 21) + (imm << 12) + getrandbits(12)
+        y = imm
+        if negative:
+            y += ((1 << 55) - 1) << 9
+    # CBZ
+    elif (test_number >> 3) == 0b101_1010_0:
+        test_number >>= 3
+        imm = (negative << 18) + getrandbits(18)
+        a = (test_number << 24) + (imm << 5) + getrandbits(5)
+        y = (imm << 2)
+        if negative:
+            y += ((1 << 43) - 1) << 21
+    # Not implemented instructions
+    else:
+        a = (test_number << 21) + getrandbits(21)
+        y = 0
+    
+    a = setCntBits(a, 32)
+    y = setCntBits(y, 64)
+
+    print(f"{a} {y}")
+
+
+print("// a, y_expected")
+cnt_tests = (1 << 11) # to test all possible opcodes with negative and positive immediates
+for i in range(cnt_tests):
+    for j in range(2):
+        gen(i, j)
--- SingleCycleProcessor/simple-modules/test-benches/signext_tb.sv
+++ SingleCycleProcessor/simple-modules/test-benches/signext_tb.sv
@@ -2,15 +2,17 @@
 module signext_tb ();
 
   // Parameters
-  parameter CNT_TESTS = 13;  // cnt of tests
+  parameter CNT_MAX_TESTS = 10000;  // maximum cnt of tests
 
   // Test bench variables
   logic clk, reset_tb;
   logic [63 : 0] y_expected;
 
-  int test_number, cnt_errors;
-  logic [95 : 0] test[0 : CNT_TESTS-1];
-  // {{a}, {y_expected}}
+  int fd, error_code, cnt_tests, test_number, cnt_errors;
+  string input_file_path, line;
+
+  logic [31 : 0] a_in[0 : CNT_MAX_TESTS];
+  logic [63 : 0] y_in[0 : CNT_MAX_TESTS];
 
   // Module connections
   logic [31 : 0] a;
@@ -31,44 +33,40 @@ module signext_tb ();
 
   initial begin
     // Init tests
+    cnt_tests = 0;
     test_number = 0;
     cnt_errors = 0;
 
-    test =  // {{instruction}, {expected_result}}
-    '{
-        // LDUR
-        {
-          {11'b111_1100_0010, 1'b0, 8'd251, 12'b0}, {{56{1'b0}}, 8'd251}
-        },  // positive
-        {{11'b111_1100_0010, 1'b1, 8'd251, 12'b0}, {{56{1'b1}}, 8'd251}},  // negative
-        // STUR
-        {
-          {11'b111_1100_0000, 1'b0, 8'd251, 12'b0}, {{56{1'b0}}, 8'd251}
-        },  // positive
-        {{11'b111_1100_0000, 1'b1, 8'd251, 12'b0}, {{56{1'b1}}, 8'd251}},  // negative
-        // CBZ
-        {
-          {8'b101_1010_0, 1'b0, 18'd262140, 5'b0}, {{44{1'b0}}, 18'd262140}, 2'b0
-        },  // positive
-        {{8'b101_1010_0, 1'b1, 18'd262140, 5'b0}, {{44{1'b1}}, 18'd262140}, 2'b0},  // negative
-        // Other implemented opcodes
-        {
-          {11'b100_0101_1000, {21{1'b1}}}, {64'b0}
-        },  // ADD
-        {{11'b110_0101_1000, {21{1'b1}}}, {64'b0}},  // SUB
-        {{11'b100_0101_0000, {21{1'b1}}}, {64'b0}},  // AND
-        {{11'b101_0101_0000, {21{1'b1}}}, {64'b0}},  // OR
-        // Not implemented opcodes
-        {
-          {11'b100_1101_1000, {21{1'b1}}}, {64'b0}
-        },  // MUL
-        {{9'b111_1001_01, {23{1'b1}}}, {64'b0}},  // MOVK
-        {{11'b110_0101_0000, {21{1'b1}}}, {64'b0}}  // EOR
-    };
+    // Get input file path
+    input_file_path = `__FILE__;
+    for (int i = input_file_path.len(); i >= 0; i--)
+      if (input_file_path[i] == "/") begin
+        input_file_path = input_file_path.substr(0, i);
+        break;
+      end
+
+    // Open and read input file
+    fd = $fopen({input_file_path, "input-files/signext_tb_in"}, "r");
+    if (fd === 0) begin
+      $display("ERROR when open the input file: %0d", fd);
+      $stop;
+    end
+
+    while (!$feof(
+        fd
+    ) !== 0) begin
+      error_code = $fgets(line, fd);
+      if (line === "" || line.substr(0, 1) === "//") continue;
+      error_code = $sscanf(line, "%d %d", a_in[cnt_tests], y_in[cnt_tests]);
+      cnt_tests++;
+    end
+
+    $fclose(fd);
 
     // First reset test bench creation
     reset_tb = 1;
-    #27ns reset_tb = 0;
+    #27ns;
+    reset_tb = 0;
   end
 
   always @(negedge clk) begin
@@ -76,23 +74,25 @@ module signext_tb ();
     if (~reset_tb) begin
       if (y !== y_expected) begin
         $display("Error in test number %d with \
-			input = { a = %d } and \
-			output = { y = %d } --> \
-			The expected output was { y_expected = %d }\
+			input = { a = %b } and \
+			output = { y = %b } --> \
+			The expected output was { y_expected = %b }\
 			", test_number, a, y, y_expected);
         cnt_errors++;
       end
       test_number++;
 
-      if (test_number === CNT_TESTS) begin
-        $display("%d tests completed with %d errors", CNT_TESTS, cnt_errors);
-        #5ns $stop;
+      if (test_number === cnt_tests) begin
+        $display("%d tests completed with %d errors", cnt_tests, cnt_errors);
+        #5ns;
+        $stop;
       end
     end
 
     // Prepare next test to positive edge of clk
     #2ns;
-    {a, y_expected} = test[test_number];
+    a = a_in[test_number];
+    y_expected = y_in[test_number];
     #2ns;
   end
 
