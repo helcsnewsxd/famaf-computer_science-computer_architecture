b80a35d24eca18f39b04cc562425bdcda8ebbb4a
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-09-01 16:38:05 -0300

    Mejorado test bench de regfile

---
+++ SingleCycleProcessor/simple-modules/test-benches/input-files/regfile_tb_in_gen.py
@@ -0,0 +1,32 @@
+from random import getrandbits, randint
+
+def setCntBits(number, cntBits):
+    return number & ((1 << cntBits) - 1)
+
+def printValues(we3, ra1, ra2, wa3, wd3, rd1_expected, rd2_expected):
+    we3 = setCntBits(int(we3), 1)
+    ra1 = setCntBits(ra1, 5)
+    ra2 = setCntBits(ra2, 5)
+    wa3 = setCntBits(wa3, 5)
+    wd3 = setCntBits(wd3, 64)
+    rd1_expected = setCntBits(rd1_expected, 64)
+    rd2_expected = setCntBits(rd2_expected, 64)
+    print(f"{we3} {ra1} {ra2} {wa3} {wd3} {rd1_expected} {rd2_expected}")
+
+def createTestCase(we3, ra1, ra2, wa3, wd3):
+    if we3 and wa3 != 31:
+        reg[wa3] = wd3
+    printValues(we3, ra1, ra2, wa3, wd3, reg[ra1], reg[ra2])
+
+print("// we3, ra1, ra2, wa3, wd3, rd1_expected, rd2_expected")
+reg = []
+for i in range(32):
+    reg.append(i%31)
+
+# Check initialization
+for i in range(32):
+    createTestCase(0, i, i, 0, 0)
+
+# Check writing
+for i in range(32):
+    createTestCase(1, randint(0, 31), i, i, getrandbits(64))
--- SingleCycleProcessor/simple-modules/test-benches/regfile_tb.sv
+++ SingleCycleProcessor/simple-modules/test-benches/regfile_tb.sv
@@ -2,15 +2,18 @@
 module regfile_tb ();
 
   // Parameters
-  parameter CNT_TESTS = 64;  // cnt of tests
+  parameter CNT_MAX_TESTS = 10000;  // maximum cnt of tests
 
   // Test bench variables
   logic clk, reset_tb;
   logic [63 : 0] rd1_expected, rd2_expected, wd3_auxiliar;
 
-  int test_number, cnt_errors;
-  logic [207 : 0] test[0 : CNT_TESTS-1];
-  // {{we3}, {ra1}, {ra2}, {wa3}, {wd3}, {rd1_expected}, {rd2_expected}}
+  int fd, error_code, cnt_tests, test_number, test_type, cnt_errors;
+  string input_file_path, line;
+
+  logic we3_in[0 : CNT_MAX_TESTS];
+  logic [4 : 0] ra1_in[0 : CNT_MAX_TESTS], ra2_in[0 : CNT_MAX_TESTS], wa3_in[0 : CNT_MAX_TESTS];
+  logic [63 : 0] wd3_in[0 : CNT_MAX_TESTS], rd1_in[0 : CNT_MAX_TESTS], rd2_in[0 : CNT_MAX_TESTS];
 
   // Module connections
   logic we3;
@@ -38,56 +41,102 @@ module regfile_tb ();
 
   initial begin
     // Init tests
+    cnt_tests = 0;
     test_number = 0;
-    cnt_errors  = 0;
+    cnt_errors = 0;
 
-    // test-check initialization
-    for (int i = 0; i < 32; i++) begin
-      wd3_auxiliar = {2{32'($urandom())}};
-      test[i] = {{1'b0}, {3{5'(i)}}, {wd3_auxiliar}, {2{(i === 0 || i === 31 ? 64'd0 : 64'(i))}}};
+    // Get input file path
+    input_file_path = `__FILE__;
+    for (int i = input_file_path.len(); i >= 0; i--)
+      if (input_file_path[i] == "/") begin
+        input_file_path = input_file_path.substr(0, i);
+        break;
+      end
+
+    // Open and read input file
+    fd = $fopen({input_file_path, "input-files/regfile_tb_in"}, "r");
+    if (fd === 0) begin
+      $display("ERROR when open the input file: %0d", fd);
+      $stop;
     end
 
-    // test-check writing
-    for (int i = 0; i < 32; i++) begin
-      wd3_auxiliar = {2{32'($urandom())}};
-      test[i+32]   = {{1'b1}, {3{5'(i)}}, {wd3_auxiliar}, {2{(i !== 31 ? wd3_auxiliar : 64'b0)}}};
+    while (!$feof(
+        fd
+    ) !== 0) begin
+      error_code = $fgets(line, fd);
+      if (line === "" || line === "\n" || line.substr(0, 1) === "//") continue;
+      error_code = $sscanf(
+          line,
+          "%d %d %d %d %d %d %d",
+          we3_in[cnt_tests],
+          ra1_in[cnt_tests],
+          ra2_in[cnt_tests],
+          wa3_in[cnt_tests],
+          wd3_in[cnt_tests],
+          rd1_in[cnt_tests],
+          rd2_in[cnt_tests]
+      );
+      cnt_tests++;
     end
+
+    $fclose(fd);
 
     // First reset test bench creation
     reset_tb = 1;
-    #27ns reset_tb = 0;
+    #27ns;
+    reset_tb = 0;
   end
 
   always @(negedge clk) begin
     // Check test executed on positive edge of clk
     if (~reset_tb) begin
+      test_type = 0;
       // Test writing (reading the written register)
-      if (test_number >= 32) we3 = 1'b0;
+      if (we3 === 1'b1) begin
+        test_type = 1;
+        we3 = 1'b0;
+      end
 
       if ({rd1, rd2} !== {rd1_expected, rd2_expected}) begin
-        if (test_number < 32) $display("Error in initialization test number %d with \
-			  input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and \
-			  output = { rd1 = %d, rd2 = %d } --> \
-			  The expected output was { rd1_expected = %d, rd2_expected = %d }\
-			  ", test_number, we3, ra1, ra2, wa3, wd3, rd1, rd2, rd1_expected, rd2_expected);
-        else $display("Error in writing test number %d with \
-			  input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and \
-			  output = { rd1 = %d, rd2 = %d } --> \
-			  The expected output was { rd1_expected = %d, rd2_expected = %d }\
-			  ", test_number, 1'b1, ra1, ra2, wa3, wd3, rd1, rd2, rd1_expected, rd2_expected);
+        if (test_type === 0)
+          $display(
+              "Error in %s test number %d with \
+			        input = { we3 = %b, ra1 = %d, ra2 = %d, wa3 = %d, wd3 = %d } and \
+			        output = { rd1 = %d, rd2 = %d } --> \
+			        The expected output was { rd1_expected = %d, rd2_expected = %d }\
+			        ",
+              (test_type ? "writing" : "initialization"),
+              test_number,
+              (test_type ? 1'b1 : 1'b0),
+              ra1,
+              ra2,
+              wa3,
+              wd3,
+              rd1,
+              rd2,
+              rd1_expected,
+              rd2_expected
+          );
         cnt_errors++;
       end
       test_number++;
 
-      if (test_number === CNT_TESTS) begin
-        $display("%d tests completed with %d errors", CNT_TESTS, cnt_errors);
-        #5ns $stop;
+      if (test_number === cnt_tests) begin
+        $display("%d tests completed with %d errors", cnt_tests, cnt_errors);
+        #5ns;
+        $stop;
       end
     end
 
     // Prepare next test to positive edge of clk
     #2ns;
-    {we3, ra1, ra2, wa3, wd3, rd1_expected, rd2_expected} = test[test_number];
+    we3 = we3_in[test_number];
+    ra1 = ra1_in[test_number];
+    ra2 = ra2_in[test_number];
+    wa3 = wa3_in[test_number];
+    wd3 = wd3_in[test_number];
+    rd1_expected = rd1_in[test_number];
+    rd2_expected = rd2_in[test_number];
     #2ns;
   end
 
