a8ffe659259c8047ab92633a09ebc479d2bba38f
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-09-06 09:09:27 -0300

    Parametrizado execute

--- SingleCycleProcessor/simple-modules/execute.sv
+++ SingleCycleProcessor/simple-modules/execute.sv
@@ -1,38 +1,40 @@
 // Execute module
-module execute (
+module execute #(
+    parameter N = 64
+) (
     input logic AluSrc,
     input logic [3 : 0] AluControl,
-    input logic [63 : 0] PC_E,
+    input logic [N-1 : 0] PC_E,
     signImm_E,
     readData1_E,
     readData2_E,
-    output logic [63 : 0] PCBranch_E,
+    output logic [N-1 : 0] PCBranch_E,
     aluResult_E,
     writeData_E,
     output logic zero_E
 );
 
-  logic [63 : 0] y0_internal, y1_internal;
+  logic [N-1 : 0] y0_internal, y1_internal;
 
-  sl2 #(64) Shif_left_2 (
+  sl2 #(N) Shif_left_2 (
       .a(signImm_E),
       .y(y0_internal)
   );
 
-  adder #(64) Add (
+  adder #(N) Add (
       .a(PC_E),
       .b(y0_internal),
       .y(PCBranch_E)
   );
 
-  mux2 #(64) MUX (
+  mux2 #(N) MUX (
       .d0(readData2_E),
       .d1(signImm_E),
       .s (AluSrc),
       .y (y1_internal)
   );
 
-  alu ALU (
+  alu #(N) ALU (
       .a(readData1_E),
       .b(y1_internal),
       .ALUControl(AluControl),
--- SingleCycleProcessor/simple-modules/test-benches/execute_tb.sv
+++ SingleCycleProcessor/simple-modules/test-benches/execute_tb.sv
@@ -3,10 +3,11 @@ module execute_tb ();
 
   // Parameters
   parameter CNT_MAX_TESTS = 10000;  // maximum cnt of tests
+  parameter N = 64;
 
   // Test bench variables
   logic clk, reset_tb;
-  logic [63 : 0] PCBranch_E_expected, aluResult_E_expected, writeData_E_expected;
+  logic [N-1 : 0] PCBranch_E_expected, aluResult_E_expected, writeData_E_expected;
   logic zero_E_expected;
 
   int fd, error_code, cnt_tests, test_number, cnt_errors;
@@ -14,7 +15,7 @@ module execute_tb ();
 
   logic AluSrc_in[0:CNT_MAX_TESTS], zero_E_in[0:CNT_MAX_TESTS];
   logic [3 : 0] AluControl_in[0:CNT_MAX_TESTS];
-  logic [63 : 0]
+  logic [N-1 : 0]
       PC_E_in[0:CNT_MAX_TESTS],
       signImm_E_in[0:CNT_MAX_TESTS],
       readData1_E_in[0:CNT_MAX_TESTS],
@@ -26,9 +27,9 @@ module execute_tb ();
   // Module connections
   logic AluSrc, zero_E;
   logic [3 : 0] AluControl;
-  logic [63 : 0] PC_E, signImm_E, readData1_E, readData2_E, PCBranch_E, aluResult_E, writeData_E;
+  logic [N-1 : 0] PC_E, signImm_E, readData1_E, readData2_E, PCBranch_E, aluResult_E, writeData_E;
 
-  execute dut (
+  execute #(N) dut (
       .AluSrc(AluSrc),
       .AluControl(AluControl),
       .PC_E(PC_E),
