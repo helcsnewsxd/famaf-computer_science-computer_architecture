c2c745c1b872f2b3c72fafcabe385ffebdd8d617
helcsnewsxd <helcsnewsxd@helcsnewsxd>
2023-09-06 13:45:58 -0300

    Agregados modulos de patterson para prac2 y hecho .py para revisar r ápidamente el funcionamiento del procesador sin necesidad de ir al archivo

---
+++ SingleCycleProcessor/patterson-modules/test-benches/processor_tb_check.py
@@ -0,0 +1,9 @@
+file = open("../../simulation/modelsim/mem.dump")
+
+for i in range(2):
+    print(file.readline())
+for i in range(22):
+    s = file.readline()
+    number = int("0b"+(s.split(" ")[1].split("\n")[0]), 2)
+    print(f"{i} {hex(number)}")
+file.close()
